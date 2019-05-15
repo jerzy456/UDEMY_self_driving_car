b56e009cd4c34cbf4e484d5b7b4263eacd5c2af6
Jerzy <jerzy2baranowski@gmail.com>
2019-05-15 12:51:21 +0200

    implemented sigmoid fixed errors

--- Perceptron/LogisticRegression.py
+++ Perceptron/LogisticRegression.py
@@ -1,28 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def draw(x1,x2):
+    ln = plt.plot(x1,x2)
+
+def sigmoid (score):
+    return 1/(1+ np.exp(-score))
+
 np.random.seed(0)
-n_pts=100
-b=np.ones(n_pts)
+n_pts=5
+bias=np.ones(n_pts)
 
 rand_x2_val = np.random.normal(4, 2, n_pts)
 rand_y2_val = np.random.normal(6, 2, n_pts)
-top_region = np.array([np.random.normal(10, 2, n_pts),np.random.normal(12, 2, n_pts), b]).T
-low_region = np.array([np.random.normal(5, 2, n_pts),np.random.normal(6, 2, n_pts), b]).T
+top_region = np.array([np.random.normal(10, 2, n_pts),np.random.normal(12, 2, n_pts), bias]).T
+low_region = np.array([np.random.normal(5, 2, n_pts),np.random.normal(6, 2, n_pts), bias]).T
 all_points=np.vstack((top_region, low_region))
 
 w1= -0.2
 w2= -0.35
-bias = 3.5
-line_parameters = np.matrix([w1, w2, bias])
-x1 = np.arrey([low_region[:, 0].min(), low_region[:, 0].max()])
-#w1x1+w2x2+b=0
-#x2= -b/w2 +x1*(-w1/w2)
-x2= -b /w2 +x1 * (-w1/w2)
+b = 3.5
+line_paramters = np.matrix([w1,w2,b]).T
+x1=np.array([low_region[:,0].min(), top_region[:,0].max()])
+x2= -b/w2 + (x1*(-w1/w2))
 
+linear_combination= all_points*line_paramters
+probabilities= sigmoid(linear_combination)
+print(probabilities)
 
 #plot stuff
 plt.subplot(111)
 plt.scatter(top_region[:,0], top_region[:,1], color='r')
 plt.scatter(low_region[:,0], low_region[:,1],color='b')
+draw(x1,x2)
 plt.show()
