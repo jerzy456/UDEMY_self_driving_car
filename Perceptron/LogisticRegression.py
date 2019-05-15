7fd2a0878cec977afb13ce164bd7e24e88addff2
Jerzy <jerzy2baranowski@gmail.com>
2019-05-15 16:23:12 +0200

    Finnished classificatior with cross entropy No training

--- Perceptron/LogisticRegression.py
+++ Perceptron/LogisticRegression.py
@@ -13,6 +13,14 @@ def calculate_error(line_parameters, points, y):
     m=points.shape[0]
     return (np.log(p).T*y +np.log(1-p).T*(1-y))*(1/m)
 
+def gradient_decent(line_parameters, points, y, alpha):
+    m = points.shape[0]
+    alpha = 0.01
+    for i in range(500):
+        p = sigmoid(points*line_parameters)
+        grad = (points.T * (p-y))*(alpha/m)
+        line_parameters = line_parameters - grad
+
 
 np.random.seed(0)
 n_pts=10
@@ -24,12 +32,9 @@ top_region = np.array([np.random.normal(10, 2, n_pts),np.random.normal(12, 2, n_
 low_region = np.array([np.random.normal(5, 2, n_pts),np.random.normal(6, 2, n_pts), bias]).T
 all_points=np.vstack((top_region, low_region))
 
-w1= -0.2
-w2= -0.35
-b = 3.5
-line_parameters = np.matrix([w1,w2,b]).T
-x1=np.array([low_region[:,0].min(), top_region[:,0].max()])
-x2= -b/w2 + (x1*(-w1/w2))
+
+line_parameters = np.matrix([np.zeros(3)]).T
+
 
 linear_combination= all_points*line_parameters
 y = np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts*2,1)
@@ -40,5 +45,5 @@ print(cross_error)
 plt.subplot(111)
 plt.scatter(top_region[:,0], top_region[:,1], color='r')
 plt.scatter(low_region[:,0], low_region[:,1],color='b')
-draw(x1,x2)
+
 plt.show()
