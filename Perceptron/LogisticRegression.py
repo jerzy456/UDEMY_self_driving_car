b8ea490ee33cd62eee8ee8c1fe67bee45be022ae
Jerzy <jerzy2baranowski@gmail.com>
2019-05-16 09:30:58 +0200

    Finnished perceptron

--- Perceptron/LogisticRegression.py
+++ Perceptron/LogisticRegression.py
@@ -9,26 +9,30 @@ def sigmoid (score):
     return 1/(1+ np.exp(-score))
 
 def calculate_error(line_parameters, points, y):
-    p = sigmoid(linear_combination)
+    p = sigmoid(points*line_parameters)
     m=points.shape[0]
     return (np.log(p).T*y +np.log(1-p).T*(1-y))*(1/m)
 
 def gradient_decent(line_parameters, points, y, alpha):
-    m = points.shape[0]
+    n = points.shape[0]
     alpha = 0.01
-    for i in range(500):
-        p = sigmoid(points*line_parameters)
-        grad = (points.T * (p-y))*(alpha/m)
-        line_parameters = line_parameters - grad
-        w1= line_parameters.item(0)
+    for i in range(4000):
+        p = sigmoid(points * line_parameters)
+        gradient = points.T * (p - y) * (alpha / n)
+        line_parameters = line_parameters - gradient
+
+        w1 = line_parameters.item(0)
         w2 = line_parameters.item(1)
         b = line_parameters.item(2)
 
-        x1 = np.array([points[:,0].min(),points[:,0].max()])
-        x2 = - b/w2 +x1 *( - w1 /w2)
+        x1 = np.array([points[:, 0].min(), points[:, 0].max()])
+        x2 = -b / w2 + (x1 * (-w1 / w2))
+    draw(x1,x2)
+    print(calculate_error(line_parameters,points, y))
+    return line_parameters
 
 np.random.seed(0)
-n_pts=10
+n_pts=50
 bias=np.ones(n_pts)
 
 rand_x2_val = np.random.normal(4, 2, n_pts)
@@ -37,11 +41,10 @@ top_region = np.array([np.random.normal(10, 2, n_pts),np.random.normal(12, 2, n_
 low_region = np.array([np.random.normal(5, 2, n_pts),np.random.normal(6, 2, n_pts), bias]).T
 all_points=np.vstack((top_region, low_region))
 
-
 line_parameters = np.matrix([np.zeros(3)]).T
-gradient_decent(line_parameters, )
+y = np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts*2, 1)
+line_parameters = gradient_decent(line_parameters, all_points, y, 0.06)
 
-y = np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts*2,1)
 cross_error = calculate_error(line_parameters, all_points, y)
 print(cross_error)
 
